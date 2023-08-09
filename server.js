f54d5982b66c7ddcd491aed6a7353dfa3fa35d38
Rohit Yadav <126584837+Rohit-Yadavv@users.noreply.github.com>
2023-08-09 10:38:38 +0530

    commit

--- server.js
+++ server.js
@@ -1,3 +1,83 @@
+// import dotenv from 'dotenv';
+// import express from 'express';
+// import morgan from 'morgan';
+// import connectDB from './config/db.js';
+// import colors from 'colors';
+// import authrouter from './routes/authRoutes.js';
+// import blogrouter from './routes/blogroutes.js';
+// import categoryrouter from './routes/categoryRoutes.js';
+// import contactRouter from './routes/contactRoutes.js';
+// import connectRouter from './routes/connectRoutes.js';
+// import cors from 'cors';
+
+// // for deployment
+
+// import path from 'path';
+// import { fileURLToPath } from 'url';
+
+
+// // Load environment variables from .env file
+// dotenv.config();
+
+
+
+
+// // es6 fix we can't use __dirname in es6
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+
+
+
+// // Create the Express app
+// const app = express();
+
+// // Connect to the MongoDB database
+// connectDB();
+
+
+
+// // for deployment
+// app.use(express.static(path.join(__dirname, "./frontend/build")));
+
+
+
+
+// // Middlewares
+// // Log HTTP requests in development mode
+// app.use(morgan('dev'));
+// // Parse incoming JSON data in the request body
+// app.use(express.json());
+// // Enable Cross-Origin Resource Sharing (CORS) for all routes
+// app.use(cors());
+
+
+
+// // Routes
+// // Routes for authentication
+// app.use('/api/v1/auth', authrouter);
+// // Routes for blogs
+// app.use('/api/v1/blog', blogrouter);
+// // Routes for categories
+// app.use('/api/v1/category', categoryrouter);
+// // Routes for contact
+// app.use('/api/v1/contact', contactRouter);
+// // Routes for connection
+// app.use('/api/v1/connect', connectRouter);
+
+// // rest api deployment
+// app.use("*", function (req, res) {
+//     res.sendFile(path.join(__dirname, "./frontend/build/index.html"))
+// })
+
+
+// // Running the server
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//     console.log(`Server running at ${PORT}`);
+// });
+
+
 import dotenv from 'dotenv';
 import express from 'express';
 import morgan from 'morgan';
@@ -10,38 +90,11 @@ import contactRouter from './routes/contactRoutes.js';
 import connectRouter from './routes/connectRoutes.js';
 import cors from 'cors';
 
-// for deployment
-
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-
 // Load environment variables from .env file
 dotenv.config();
 
-
-
-
-// es6 fix we can't use __dirname in es6
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-
-
-
 // Create the Express app
 const app = express();
-
-// Connect to the MongoDB database
-connectDB();
-
-
-
-// for deployment 
-app.use(express.static(path.join(__dirname, "./frontend/build")));
-
-
-
 
 // Middlewares
 // Log HTTP requests in development mode
@@ -50,7 +103,6 @@ app.use(morgan('dev'));
 app.use(express.json());
 // Enable Cross-Origin Resource Sharing (CORS) for all routes
 app.use(cors());
-
 
 
 // Routes
@@ -65,14 +117,11 @@ app.use('/api/v1/contact', contactRouter);
 // Routes for connection
 app.use('/api/v1/connect', connectRouter);
 
-// rest api deployment
-app.use("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "./frontend/build/index.html"))
-})
 
-
-// Running the server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server running at ${PORT}`);
-});
+//Connect to the database before listening
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log("listening for requests");
+    })
+})
